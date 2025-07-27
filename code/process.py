bb2b108a2efae7c6ce9adf6207a0e9a3693508ff
Patrick Williamson <patrick@truepathvision.com>
2025-07-27 16:58:40 -0400

    updating

--- code/process.py
+++ code/process.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def parse_file(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
@@ -6,4 +7,12 @@ def parse_file(filepath):
 
     words = re.findall(r"\b\w+\b", text.lower())
     return words
+
+def get_paths(root_dir='/home/tpv/Random/Lang/datasets/fullEnglish'):
+    all_paths = []
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+            all_paths.append(full_path)
+    return all_paths
 
