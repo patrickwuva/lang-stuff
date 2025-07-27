762d6f7be3dccc452e0070678a88cf8bb82e28ab
Patrick Williamson <patrick@truepathvision.com>
2025-07-27 17:11:28 -0400

    updating

--- code/main.py
+++ code/main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     total_words = 0
     with ProcessPoolExecutor(max_workers=64) as executor:
         results = executor.map(parse_file, paths, chunksize=10)
-        for words in tqdm(results, total=len(paths), desc="Parsing", unit="file"):
-            total_words += len(words)
+        for word_count in tqdm(results, total=len(paths), desc="Parsing", unit="file"):
+            total_words += word_count
 
     print(f"Total words: {total_words}")
 
