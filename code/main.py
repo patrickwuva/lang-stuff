27b74b6fc642ee6bc95966bf5c3356bb679dc4c2
Patrick Williamson <patrick@truepathvision.com>
2025-07-27 17:05:09 -0400

    updating

--- code/main.py
+++ code/main.py
@@ -1,6 +1,5 @@
-from glob import glob
 from process import parse_file, get_paths
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 
 if __name__ == '__main__':
@@ -8,9 +7,15 @@ if __name__ == '__main__':
     print(f"Found {len(paths)} files.")
 
     total_words = 0
-    with ThreadPoolExecutor(max_workers=32) as executor:
-        for words in tqdm(executor.map(parse_file, paths), total=len(paths)):
-            total_words += len(words)
+    with ThreadPoolExecutor(max_workers=64) as executor:
+        futures = [executor.submit(parse_file, path) for path in paths]
+
+        for future in tqdm(as_completed(futures), total=len(futures), desc="Parsing"):
+            try:
+                words = future.result()
+                total_words += len(words)
+            except Exception as e:
+                print(f"Error: {e}")
 
     print(f"Total words: {total_words}")
 
