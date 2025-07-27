31f686f74948fc76b9df60a216e1ab4e31009981
Patrick Williamson <patrick@truepathvision.com>
2025-07-27 17:38:43 -0400

    updating

--- code/main.py
+++ code/main.py
@@ -1,20 +1,26 @@
-from process import parse_file, get_paths, count_letters_in_text
+from process import parse_file, get_paths, count_letters_in_text, merge_results
 from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
 from dataclasses import asdict
+from result import Result
+
+def process_file(filepath):
+    words = parse_file(filepath)
+    return count_letters_in_text(words)
 
 if __name__ == '__main__':
     paths = get_paths()
     print(f"Found {len(paths)} files.")
 
-    total_words = 0
-    """
+    master_result = Result()
+
     with ProcessPoolExecutor(max_workers=64) as executor:
-        results = executor.map(parse_file, paths, chunksize=3)
-        for word_count in tqdm(results, total=len(paths), desc="Parsing", unit="file"):
-            total_words += word_count
-    """
-    text = parse_file(paths[0])
-    r = count_letters_in_text(text)
-    #print(f"Total words: {total_words}")
-    print(asdict(r))
+        partial_results = executor.map(process_file, paths, chunksize=3)
+        for r in tqdm(partial_results, total=len(paths), desc="Counting Letters", unit="file"):
+            master_result = merge_results(master_result, r)
+
+    print("Final Letter Frequencies:")
+    for ch, count in asdict(master_result).items():
+        if count > 0:
+            print(f"{ch}: {count}")
+
